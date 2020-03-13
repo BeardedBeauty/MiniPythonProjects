296cfd20ae1d76d03a2bd66fc24aeeda624a8d86
John Ochs <john.w.ochs0@gmail.com>
2020-03-13 16:52:41 -0500

    missing ) ha wow it's almost like im a beginner

--- scraper.py
+++ scraper.py
@@ -4,14 +4,7 @@ from bs4 import BeautifulSoup
 q = requests.get("https://weather.com/weather/today/l/55347:4:US")
 w = BeautifulSoup(q.content, "html.parser").find(id="LookingAhead")
 
-r = []
-y = w.find(classname="today-daypart-content")
-for t in y:
-    print(t)
-    # r.append(y[t])
-
-
-# print(w.find(class="today-daypart-temp")
+print(w.find(classname="today-daypart-content").find(class_="today-daypart-temp"))
 
 # e = {
 #     "today": {
