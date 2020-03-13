818ed6ec4f21c9f5987d7f8260391417790d7dfc
John Ochs <john.w.ochs0@gmail.com>
2020-03-13 16:36:11 -0500

    python is weird about loops

--- scraper.py
+++ scraper.py
@@ -2,6 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 
 q = requests.get("https://weather.com/weather/today/l/55347:4:US")
-w = BeautifulSoup(q.content, "html.parser")
+w = BeautifulSoup(q.content, "html.parser").find(id="LookingAhead")
 
-print(w)
+r = []
+y = w.find(classname="today-daypart-content")
+for t in y:
+    print(t)
+    # r.append(y[t])
+
+
+# print(w.find(class="today-daypart-temp")
+
+# e = {
+#     "today": {
+#         "highlow": w.find(id="dp0-highLow")
+#         "Ftemp": w.find(class="today-daypart-temp").find("span")
+#     },
+#     "tomorrow": w.find(),
+
+# }
