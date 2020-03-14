1149bc291782208ff87946c15a37bf15bd2a77c1
John Ochs <john.w.ochs0@gmail.com>
2020-03-14 12:39:20 -0500

    completed scraping of weather, currently scrapes today temps, low/high

--- scraper.py
+++ scraper.py
@@ -1,16 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 q = requests.get("https://weather.com/weather/today/l/55347:4:US")
 w = BeautifulSoup(q.content, "html.parser").find(id="LookingAhead")
+y = w.find(class_="today-daypart-content")
+e = re.search("[0-9]+", str(y.find(class_="")))
+r = re.search(">High<|>Low<", str(y.find(id="dp0-highLow")))
 
-print(w.find(classname="today-daypart-content").find(class_="today-daypart-temp"))
 
-# e = {
-#     "today": {
-#         "highlow": w.find(id="dp0-highLow")
-#         "Ftemp": w.find(class="today-daypart-temp").find("span")
-#     },
-#     "tomorrow": w.find(),
+class Day:
+    def __init__(d, days, temp, lh):
+        d.days = days
+        d.temp = temp
+        d.lh = lh
 
-# }
+
+today = Day("today", e.group(), r.group())
+print(today.days + today.temp + today.lh)
